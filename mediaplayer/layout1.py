c020a79091b1dfc0160226b66b846d7d8092666c
Victor Motha <victorsmotha@gmail.com>
2016-05-29 12:02:40 +0200

    updated play upload module.

--- mediaplayer/layout1.py
+++ mediaplayer/layout1.py
@@ -1,49 +1,51 @@
 from Tkinter import Tk, RIGHT, BOTH, RAISED, LEFT, CENTER, TOP, BOTTOM
-from ttk import Frame, Button, Style
+from ttk import Frame, Button, Style, Progressbar
 import pygame
 from tkFileDialog import askopenfilename
 
 
+
 class ControlDesigns(Frame):
-  
+
     def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
+        Frame.__init__(self, parent)
+
         self.parent = parent
         self.initUI()
-        
+
     def initUI(self):
-      
-        self.parent.title("Buttons")
+
+        self.parent.title("jam arrange V.0.1")
         self.style = Style()
         self.style.theme_use("default")
-        
+        '''
         frame = Frame(self, relief=RAISED, borderwidth=2)
         frame.pack(fill=BOTH, expand=True)
+        '''
+
+        frame1 = Frame(self, relief=RAISED, borderwidth=2)
+        frame1.pack(fill=BOTH, expand=True)
+        songProgress = Progressbar(frame1)
+        songProgress.pack(side=BOTTOM, padx=5, pady=5)
         
         self.pack(fill=BOTH, expand=True)
 
-        playButton = Button(self, text="Play", command=PlayMusic)
+        playButton = Button(self, text="Play", width=4, command=PlayMusic)
         playButton.pack(side=LEFT, padx=5, pady=5)
-        selectSong = Button(self, text="Add song", command=songSelector)
+        selectSong = Button(self, text=" + ", width=2 , command=addSong)
         selectSong.pack(side=RIGHT, padx=5, pady=5)
-        stopButton = Button(self, text="Stop", command=StopMusic)
+        stopButton = Button(self, text="Stop", width=4, command=StopMusic)
         stopButton.pack(side=LEFT, padx=5, pady=5)
 
 def StopMusic():
         pygame.mixer.music.stop()
 
-def PlayMusic(selectedSong,songvar):
-    if songvar == True:
+def addSong():
+    playme = songSelector()
+    if (playme != ''):
         pygame.init()
         pygame.mixer.init()
-        pygame.mixer.music.load(selectedSong)
-        pygame.mixer.music.play()
-    else:
-        selectedSong = songSelector()
-        pygame.init()
-        pygame.mixer.init()
-        pygame.mixer.music.load(selectedSong)
+        pygame.mixer.music.load(playme)
         pygame.mixer.music.play()
 
 def nextSong():
@@ -51,16 +53,35 @@ def nextSong():
 
 def songSelector():
     Tk().withdraw()
-    selectedSong = askopenfilename()
-    return selectedSong
+    global globalvar
+    globalvar = askopenfilename()
+    global globalvar
+    return globalvar
 
+def PlayMusic():
+    if (globalvar != ''):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(globalvar)
+        pygame.mixer.music.play()
+    if (globalvar == ''):
+        playme = songSelector()
+        if (playme != ''):
+            pygame.init()
+            pygame.mixer.init()
+            pygame.mixer.music.load(playme)
+            pygame.mixer.music.play()
+
+def globeVariables():
+    songName = ''
+    return songName
+
+globalvar = globeVariables()
 def main():
-    songvar = False
-    selectedSong = ''
     root = Tk()
-    root.geometry("500x300+300+300")
+    root.geometry("350x150+300+300")
     app = ControlDesigns(root)
-    root.mainloop()  
+    root.mainloop()
 
 
 if __name__ == '__main__':
