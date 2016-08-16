b285ffa9a4517604ab4086b4918b5c8cbd22d974
victor motha <victorsmotha@gmail.com>
2016-08-16 08:02:49 +0200

    update audio fingerprinting.

--- audio fingerprinting/experimentation.py
+++ audio fingerprinting/experimentation.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 
 API_key = 'AxIwHZrcAz'
-root = "C:\\Users\\User\\Music\\test folder"
+root = "home/victor/music/test folder"
 path = os.path.join(root, "targetdirectory")
 all_files_in_dir = []
 all_files_dir = []
@@ -17,7 +17,8 @@ aud_files = []
 for i in range(len(all_files_in_dir)):
     if all_files_in_dir[i].endswith(('.mp3', '.wav', '.MP3', '.wma',
                                      '.WMA', '.WAV', '.mp4', '.MP4')) == True:
-        aud_files.append(all_files_dir[i] + "\\" + str(all_files_in_dir[i]))
+        # aud_files.append(all_files_dir[i] + "\\" + str(all_files_in_dir[i]))
+        aud_files.apend(os.path.join(all_files_dir[i],str(all_files_in_dir[i])))
 '''
 data inaud_data array below:
 0. path
@@ -28,7 +29,7 @@ data inaud_data array below:
 '''
 aud_data = [[],[],[],[],[]]
 for i in range(len(aud_files)):
-    print aud_files[i] 
+    print aud_files[i]
     for score, recording_id, title, artist in acoustid.match(API_key,
                                                              aud_files[i]):
         aud_data[0].append(aud_files[i])
@@ -36,5 +37,3 @@ for i in range(len(aud_files)):
         aud_data[2].append(title)
         aud_data[3].append(recording_id)
         aud_data[4].append(score)
-
-
